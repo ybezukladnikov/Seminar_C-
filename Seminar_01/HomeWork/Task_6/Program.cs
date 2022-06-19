2992ea4156b106423f5fd4585b9200eae70677b0
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-06-19 18:46:53 +0300

    Add Task 4

---
+++ Seminar_01/HomeWork/Task_6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает 
+// число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+
+Console.Write("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Input number is even");
+}
+else
+{
+    Console.WriteLine("Input number is odd");
+}
