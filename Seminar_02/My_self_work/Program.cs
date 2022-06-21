c1a0f0d5728dc42ea44adb01ecf7d952bb948fb5
Yaroslav <yaros6666@yandex.ru>
2022-06-21 12:53:56 +0300

    add new file

---
+++ Seminar_02/My_self_work/Program.cs
@@ -0,0 +1,24 @@
+﻿int what = new Random().Next(10, 99);
+Console.WriteLine(what);
+
+int num1 = what / 10;
+int num2 = what % 10;
+
+if (num1 == num2)
+{
+    Console.WriteLine("Input number is even");
+}
+
+else
+{
+
+    if (num1 > num2)
+{
+    Console.WriteLine($"Max number is {num1}");
+}
+else 
+{
+    Console.WriteLine($"Max number is {num2}");
+}
+
+}
