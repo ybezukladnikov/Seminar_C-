c3208429d1195ce2c3fe8b60d5cf86f685be4d09
Yaroslav <yaros6666@yandex.ru>
2022-06-21 14:16:01 +0300

    add new task

--- Seminar_02/My_self_work/Program.cs
+++ Seminar_02/My_self_work/Program.cs
@@ -1,24 +1,48 @@
-﻿int what = new Random().Next(10, 99);
-Console.WriteLine(what);
+﻿// Рандомно получает число и сравнивает первую цифру со второй. 
 
-int num1 = what / 10;
-int num2 = what % 10;
+// int what = new Random().Next(10, 99);
+// Console.WriteLine(what);
 
-if (num1 == num2)
+// int num1 = what / 10;
+// int num2 = what % 10;
+
+// if (num1 == num2)
+// {
+//     Console.WriteLine("Input number is even");
+// }
+
+// else
+// {
+
+//     if (num1 > num2)
+// {
+//     Console.WriteLine($"Max number is {num1}");
+// }
+// else 
+// {
+//     Console.WriteLine($"Max number is {num2}");
+// }
+
+// }
+
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+
+Console.Write("Input first number ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number ");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % num2 == 0)
 {
-    Console.WriteLine("Input number is even");
+    Console.WriteLine("multiple");
 }
 
 else
 {
-
-    if (num1 > num2)
-{
-    Console.WriteLine($"Max number is {num1}");
-}
-else 
-{
-    Console.WriteLine($"Max number is {num2}");
-}
-
+    int div = num1 % num2;
+    Console.WriteLine($"Remainder of division is {div}");
 }
