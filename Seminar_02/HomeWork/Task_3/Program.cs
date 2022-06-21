082d8eec7b598bd2106f025077bb45bef02b9711
Yaroslav <yaros6666@yandex.ru>
2022-06-21 16:51:07 +0300

    add task 3 in homework

---
+++ Seminar_02/HomeWork/Task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Input number ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numWhile = Math.Abs(num);
+int count = 0;
+
+while (numWhile > 0 )
+{
+    count ++;
+    numWhile /=10;
+}
+int[] array = new int[count];
+
+if (count < 3)
+{
+    Console.WriteLine("There is not third number");
+}
+
+else
+{
+    
+    for (int i = count-1; i >=0; i--)
+{
+    array[i] = Math.Abs(num%10);
+    num /=10;
+}
+    Console.WriteLine(array[2]);
+}
+
