eaeb2b0b7479abc66a709186b302cd8b441237da
Yaroslav <yaros6666@yandex.ru>
2022-06-21 15:24:03 +0300

    add task 1 in homework

---
+++ Seminar_02/HomeWork/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Input number ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[3];
+
+for (int i = 2; i >=0; i--)
+{
+    array[i] = num%10;
+    num /=10;
+}
+
+
+Console.WriteLine(array[1]);
+
+
+
+
+
+
