83caf38357f3e519190de939511886adc29e5698
Yaroslav <yaros6666@yandex.ru>
2022-06-21 17:39:14 +0300

    add task 2 in homework

---
+++ Seminar_02/HomeWork/Task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+
+int numWhile = Math.Abs(num);
+int count = 0;
+
+while (numWhile > 0 )
+{
+    count ++;
+    numWhile /=10;
+}
+int[] array = new int[count];
+
+ 
+for (int i = count-1; i >=0; i--)
+{
+array[i] = Math.Abs(num%10);
+num /=10;
+}
+    
+string s1 = array[0].ToString();
+string s2 = array[2].ToString();
+
+int n = Convert.ToInt32(s1+s2);
+
+Console.WriteLine(n);
