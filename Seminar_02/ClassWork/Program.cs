2e334debf888e784d7b632be553ba0107d1ad966
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-06-25 10:34:42 +0300

    Add new task

--- Seminar_02/ClassWork/Program.cs
+++ Seminar_02/ClassWork/Program.cs
@@ -1,20 +1,83 @@
 ﻿// Рандомно получает число и сравнивает первую цифру со второй. 
 
-int what = new Random().Next(10, 99);
-void maxMin()
-{
-    int arg = new Random().Next(10, 100);
-    Console.WriteLine("Current number is "+ arg);
-    int num1 = arg / 10;
-    int num2 = arg % 10;
-    int max = num1;
+// int what = new Random().Next(10, 99);
+// void maxMin()
+// {
+//     int arg = new Random().Next(10, 100);
+//     Console.WriteLine("Current number is "+ arg);
+//     int num1 = arg / 10;
+//     int num2 = arg % 10;
+//     int max = num1;
 
-    if (num2 > max) max = num2;
-    Console.WriteLine("Bigger digit  is " + max);
+//     if (num2 > max) max = num2;
+//     Console.WriteLine("Bigger digit  is " + max);
+
+// }
+
+// maxMin();
+
+// Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int Delete(int arg)
+{
+    int numWhile = arg;
+    int count = 0;
+
+    while (numWhile > 0 )
+    {
+        count ++;
+        numWhile /=10;
+    }
+    int[] array = new int[count];
+
+    
+    for (int i = count-1; i >=0; i--)
+    {
+    array[i] = num%10;
+    num /=10;
+    }
+        
+    string s1 = array[0].ToString();
+    string s2 = array[2].ToString();
+
+    int n = Convert.ToInt32(s1+s2);
+    return n;
 
 }
 
-maxMin();
+int test = Delete(num);
+Console.WriteLine(test);
+
+
+
+// int numWhile = num;
+// int count = 0;
+
+// while (numWhile > 0 )
+// {
+//     count ++;
+//     numWhile /=10;
+// }
+// int[] array = new int[count];
+
+ 
+// for (int i = count-1; i >=0; i--)
+// {
+// array[i] = num%10;
+// num /=10;
+// }
+    
+// string s1 = array[0].ToString();
+// string s2 = array[2].ToString();
+
+// int n = Convert.ToInt32(s1+s2);
+
+// Console.WriteLine(n);
 
 
 // Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
