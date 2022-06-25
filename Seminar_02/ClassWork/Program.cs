a83c4ea110506032e8ec0aab94f671de27257f9f
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-06-25 10:46:27 +0300

    Add new task

--- Seminar_02/ClassWork/Program.cs
+++ Seminar_02/ClassWork/Program.cs
@@ -20,75 +20,69 @@
 // и удаляет вторую цифру этого числа.
 
 
-int num = new Random().Next(100, 1000);
-Console.WriteLine(num);
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine(num);
 
-int Delete(int arg)
-{
-    int numWhile = arg;
-    int count = 0;
+// int Delete(int arg)
+// {
+//     int numWhile = arg;
+//     int count = 0;
 
-    while (numWhile > 0 )
-    {
-        count ++;
-        numWhile /=10;
-    }
-    int[] array = new int[count];
+//     while (numWhile > 0 )
+//     {
+//         count ++;
+//         numWhile /=10;
+//     }
+//     int[] array = new int[count];
 
     
-    for (int i = count-1; i >=0; i--)
-    {
-    array[i] = num%10;
-    num /=10;
-    }
+//     for (int i = count-1; i >=0; i--)
+//     {
+//     array[i] = num%10;
+//     num /=10;
+//     }
         
-    string s1 = array[0].ToString();
-    string s2 = array[2].ToString();
+//     string s1 = array[0].ToString();
+//     string s2 = array[2].ToString();
 
-    int n = Convert.ToInt32(s1+s2);
-    return n;
+//     int n = Convert.ToInt32(s1+s2);
+//     return n;
 
+// }
+
+// int test = Delete(num);
+// Console.WriteLine(test);
+
+
+// Напишите программу, которая будет принимать на вход два 
+// числа и выводить, является ли второе число кратным первому. 
+
+
+
+Console.Write("Input first number ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number ");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+void multiple(int arg1, int arg2)
+{
+    if (num1 % num2 == 0)
+    {
+        Console.WriteLine("multiple");
+    }
+
+    else
+    {
+        int div = num1 % num2;
+        Console.WriteLine($"Remainder of division is {div}");
+    }    
 }
 
-int test = Delete(num);
-Console.WriteLine(test);
+multiple(num1, num2);
 
-
-
-// int numWhile = num;
-// int count = 0;
-
-// while (numWhile > 0 )
-// {
-//     count ++;
-//     numWhile /=10;
-// }
-// int[] array = new int[count];
-
- 
-// for (int i = count-1; i >=0; i--)
-// {
-// array[i] = num%10;
-// num /=10;
-// }
-    
-// string s1 = array[0].ToString();
-// string s2 = array[2].ToString();
-
-// int n = Convert.ToInt32(s1+s2);
-
-// Console.WriteLine(n);
-
-
-// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
-
-// Console.Write("Input first number ");
-
-// int num1 = Convert.ToInt32(Console.ReadLine());
-
-// Console.Write("Input second number ");
-
-// int num2 = Convert.ToInt32(Console.ReadLine());
 
 // if (num1 % num2 == 0)
 // {
