f3ea4855d7de84f2dc739bf3d80776d209aeb81d
Yaroslav <yaros6666@yandex.ru>
2022-06-28 12:35:43 +0300

    change task 13

--- Seminar_02/HomeWork/Task_13/Program.cs
+++ Seminar_02/HomeWork/Task_13/Program.cs
@@ -9,67 +9,112 @@ Console.Write("Input number ");
 
 int num = Convert.ToInt32(Console.ReadLine());
 
-// void thirdNum(int arg)
-// {
-//     int numWhile = Math.Abs(num);
-//     int count = 0;
+void thirdNum(int arg)
+{
+    int numWhile = Math.Abs(num);
+    int count = 0;
 
-//     while (numWhile > 0 )
-//     {
-//         count ++;
-//         numWhile /=10;
-//     }
-//     int[] array = new int[count];
+    while (numWhile > 0 )
+    {
+        count ++;
+        numWhile /=10;
+    }
+    int[] array = new int[count];
 
-//     if (count < 3)
-//     {
-//         Console.WriteLine("There is not third number");
-//     }
+    if (count < 3)
+    {
+        Console.WriteLine("There is not third number");
+    }
 
-//     else
-//     {
+    else
+    {
         
-//         for (int i = count-1; i >=0; i--)
-//     {
-//         array[i] = Math.Abs(num%10);
-//         num /=10;
-//     }
-//         Console.WriteLine(array[2]);
-//     }
-// }
+        for (int i = count-1; i >=0; i--)
+    {
+        array[i] = Math.Abs(num%10);
+        num /=10;
+    }
+        Console.WriteLine(array[2]);
+    }
+}
 
-// thirdNum(num);
+thirdNum(num);
 
 
 // Второй способ. Делением. 
 
+// Console.Write("Input number ");
 
-void thirdNum(int arg)
-{
-    double argAbs = Math.Abs(arg);
-    double interResult=0;
-    int interResult_1;
+// int num = Convert.ToInt32(Console.ReadLine());
 
-    if (argAbs < 100)
-    {
-        Console.WriteLine("There is not third number");
-    }
+
+// void thirdNum(int arg)
+// {
+//     double argAbs = Math.Abs(arg);
+//     double interResult=0;
+//     int interResult_1;
+
+//     if (argAbs < 100)
+//     {
+//         Console.WriteLine("There is not third number");
+//     }
         
-    else 
-    {
-        while (argAbs>1)
-        {
-            argAbs/=10;
-            interResult = argAbs;
+//     else 
+//     {
+//         while (argAbs>1)
+//         {
+//             argAbs/=10;
+//             interResult = argAbs;
             
-        }
-        interResult *=1000;
-        interResult_1 = (int)interResult;
-        int result = interResult_1%10;
-        Console.WriteLine($"Third number is {result}");
+//         }
+//         interResult *=1000;
+//         interResult_1 = (int)interResult;
+//         int result = interResult_1%10;
+//         Console.WriteLine($"Third number is {result}");
 
-    }    
-}
+//     }    
+// }
 
-thirdNum(num);
+// thirdNum(num);
+
+// Третий способ через строку. 
+
+// Console.Write("Input number ");
+
+// string num = Console.ReadLine();
+
+// void thirdNumString(string arg)
+// {
+//     int count = arg.Length;
+//     string firstChar = arg[0].ToString();
+
+//     if (firstChar == "-" )
+//     {
+        
+//         if (count<4)
+//         {
+//             Console.WriteLine("There is not third number");
+//         }
+//         else
+//         {
+//             Console.WriteLine(arg[3]);
+//         }
+        
+//     }
+
+//     else
+//     {
+//         if (count<3)
+//         {
+//             Console.WriteLine("There is not third number");
+//         }
+//         else
+//         {
+//             Console.WriteLine(arg[2]);
+//         }
+//     }
+// }
+// thirdNumString(num);
+
+
 
