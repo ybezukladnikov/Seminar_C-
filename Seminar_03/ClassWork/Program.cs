65cc8ff7357f7aa726b735cd54dd8b190390c710
Yaroslav <yaros6666@yandex.ru>
2022-06-29 11:45:39 +0300

    add class work

---
+++ Seminar_03/ClassWork/Program.cs
@@ -0,0 +1,15 @@
+﻿void findQuarter(int arg1, int arg2)
+{
+    if (arg1>0 & arg2>0) Console.WriteLine("1 quarter");
+    if (arg1<0 & arg2>0) Console.WriteLine("2 quarter");
+    if (arg1<0 & arg2<0) Console.WriteLine("3 quarter");
+    if (arg1>0 & arg2<0) Console.WriteLine("4 quarter");
+}
+
+Console.Write("Input first coordinate: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input first coordinate: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+findQuarter(x,y);
