88de6d68fb1456edf5067fc562fc594a81e1a045
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-06-30 19:00:55 +0300

    add task 23

---
+++ Seminar_03/HomeWork/Task_23/Program.cs
@@ -0,0 +1,21 @@
+﻿// На вход принимаем число. 
+
+void cubeNum(int arg)
+{
+    for(int i=1;i<=arg;i++)
+    {
+        
+        double num = Math.Pow(i,3);
+        if(i==arg)
+        {
+            Console.Write(num);
+            break;
+        }
+        Console.Write($"{num},");
+    }
+}
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+cubeNum(num);
+
