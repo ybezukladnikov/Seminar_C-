202407685bfba03efaf533e3d50c96dce480f102
Yaroslav <yaros6666@yandex.ru>
2022-07-01 18:14:34 +0300

    add

--- Seminar_03/HomeWork/Task_23/Program.cs
+++ Seminar_03/HomeWork/Task_23/Program.cs
@@ -1,42 +1,22 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+
 void cubeNum(int arg)
 {
-    if (arg>0)
-    {
-        for(int i=1;i<=arg;i++)
+    if (arg<0) Console.Write($"{1},{0},");
+
+    for(int i=1;i<=Math.Abs(arg);i++)
     {
         
         double num = Math.Pow(i,3);
-        
-        if(i==arg)
+        if (arg<0) num = num*(-1);
+        if(i==Math.Abs(arg))
         {
             Console.Write(num);
             break;
         }
-        
         Console.Write($"{num},");
     }
-    }
-    else
-    {
-        for(int i=1;i>=arg;i--)
-    {
-        
-        double num = Math.Pow(i,3);
-        
-        if(i==arg)
-        {
-            Console.Write(num);
-            break;
-        }
-        
-        Console.Write($"{num},");
-    }
-    }
-
-
-    
 }
 
 Console.WriteLine("Input number: ");
