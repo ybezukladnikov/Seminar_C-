dabbbbc66c3f60f37d5bb7fe1ee2457835f9afee
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-07-02 15:09:03 +0300

    add

--- Seminar_04/ClassWork/Program.cs
+++ Seminar_04/ClassWork/Program.cs
@@ -1,15 +1,37 @@
 ﻿// Напишите программу, которая принимает на вход число (А) 
 // и выдаёт сумму чисел от 1 до А.
 
+// int findSum(int arg)
+// {
+//     int sum = 0;
+//     for (int i = 1; i<=arg; i++)
+//     {
+//         sum = sum + i;
+
+//     }
+//     return sum;
+
+// }
+
+// Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int sumF = findSum(num);
+// Console.WriteLine(sumF);
+
+// Напишите программу, которая принимает на вход число (А) 
+// и выдаёт количество цифр в этом числе
+
 int findSum(int arg)
 {
-    int sum = 0;
-    for (int i = 1; i<=arg; i++)
+    int count = 0;
+    for (int i = Math.Abs(arg); i>1; i=i/10)
     {
-        sum = sum + i;
+    
+        count++;
 
     }
-    return sum;
+    if (arg==0) count ++;
+    return count;
 
 }
 
@@ -17,4 +39,3 @@ Console.Write("Input number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int sumF = findSum(num);
 Console.WriteLine(sumF);
-
