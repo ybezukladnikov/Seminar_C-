dabcdd9623756c9b5aed39046eb378dedf2e0f92
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-07-02 15:16:20 +0300

    add

--- Seminar_04/ClassWork/Program.cs
+++ Seminar_04/ClassWork/Program.cs
@@ -21,16 +21,40 @@
 // Напишите программу, которая принимает на вход число (А) 
 // и выдаёт количество цифр в этом числе
 
+// int findSum(int arg)
+// {
+//     int count = 0;
+//     for (int i = Math.Abs(arg); i>1; i=i/10)
+//     {
+    
+//         count++;
+
+//     }
+//     if (arg==0) count ++;
+//     return count;
+
+// }
+
+// Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int sumF = findSum(num);
+// Console.WriteLine(sumF);
+
+
+// Напишите программу, которая принимает на вход число (А) 
+// и выдаёт произведение чисел от 1 до А.
+
+
 int findSum(int arg)
 {
-    int count = 0;
-    for (int i = Math.Abs(arg); i>1; i=i/10)
+    int count = 1;
+    for (int i = 1; i<=arg; i++)
     {
     
-        count++;
+        count*=i;
 
     }
-    if (arg==0) count ++;
+    
     return count;
 
 }
