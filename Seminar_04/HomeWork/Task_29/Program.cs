73d77d30414ba52d0a71968bd8381bfe35dedbd5
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-07-03 11:43:37 +0300

    add task 27

--- Seminar_04/HomeWork/Task_29/Program.cs
+++ Seminar_04/HomeWork/Task_29/Program.cs
@@ -2,3 +2,25 @@
 // и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+int [] CreateArray(int arg)
+{
+    int [] array = new int[arg];
+
+    for (int i = 0; i<arg; i++)
+        array[i]= new Random().Next(0,100);
+
+    return array;
+}
+
+void ShowArray(int [] arg)
+{
+    for(int i = 0; i<arg.Length; i++)
+        Console.Write(arg[i] + " ");
+}
+
+Console.Write("Input size array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int [] myArray = CreateArray(size);
+ShowArray(myArray);
