212c4cb80f18259b5c9b0cf09220f74119c08d9a
Yaroslav <yaros6666@yandex.ru>
2022-07-06 10:52:50 +0300

    add

--- Seminar_05/HomeWork/Task_34/Program.cs
+++ Seminar_05/HomeWork/Task_34/Program.cs
@@ -2,64 +2,57 @@
 // трёхзначными числами. Напишите программу, 
 // которая покажет количество чётных чисел в массиве.
 
-// short[] CreateArray(short arg)
-// {
-//     short [] array = new short[arg];
-//     Random random = new Random();
-//     for (short i = 0; i < arg; i++)
-//     {
-//         array[i] = Convert.ToInt16(random.Next(100,1000));
+short[] CreateArray(short arg)
+{
+    short [] array = new short[arg];
+    Random random = new Random();
+    for (short i = 0; i < arg; i++)
+    {
+        array[i] = Convert.ToInt16(random.Next(100,1000));
         
-//     }
+    }
 
-//     return array;
+    return array;
  
         
-// }
+}
 
-// short[] myArray = CreateArray(8);
+void ShowArray(short [] arg){
 
-// void ShowArray(short [] arg)
-// {
-//     // for (short i = 0; i<arg.Length; i++)
-//     // {
-//     //     Console.WriteLine(arg[i]);
-//     // }
-        
-
-//     foreach(short el in arg)
-//     {
-//         Console.WriteLine(arg[el]);
-//     }
-        
-// }
-
-// ShowArray(myArray);
-
-
-
-//     Console.WriteLine(myArray[i]);
-
-
-// short Count(short [] arg)
-// {
-//     short count = 0;
-//     foreach (short i in arg)
-//     {
-//         if (arg[i]%2==0)
-//             count++;
-//     }
-
-//     return count;
-// }
-
-// short count = Count(CreateArray(4));
-// Console.WriteLine(count);
-
-
-
-short [] nums = {4,4,2,1,5,6,3};
-foreach(short el in nums)
+    Console.Write("Created array: [");    
+    foreach(short el in arg)
     {
-        Console.WriteLine(nums[el]);
+        if (el == arg[arg.Length - 1])
+        {
+            Console.Write(el);
+            break;
+        }
+        Console.Write(el+", ");
     }
+    Console.Write("]");   
+    Console.WriteLine();   
+    
+}
+
+short Count(short [] arg)
+{
+    short count = 0;
+    foreach (short i in arg)
+    {
+        if (i%2==0)
+            count++;
+    }
+
+    return count;
+}
+
+
+Console.Write("Input size of array: ");
+short size = Convert.ToInt16(Console.ReadLine());
+short[] myArray = CreateArray(size);
+
+ShowArray(myArray);
+
+short count = Count(myArray);
+Console.WriteLine("The number of even numbers is "+count);
+
