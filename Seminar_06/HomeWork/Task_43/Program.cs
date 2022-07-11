9f7cdbf0204959ab4953bd136a5a04ce85c6ea75
Yaroslav <yaros6666@yandex.ru>
2022-07-11 13:01:46 +0300

    add

--- Seminar_06/HomeWork/Task_43/Program.cs
+++ Seminar_06/HomeWork/Task_43/Program.cs
@@ -2,3 +2,20 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+
+(double x, double y) FindCoordinate(double k1, double b1, double k2, double b2)
+{
+    double x = Math.Round((b2-b1)/(k1-k2),2);
+    double y = Math.Round((x*k1+b1),2);
+
+    return (x, y);
+}
+
+Console.WriteLine("Input coefficients for the first equation: ");
+Console.Write("k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+var Coordinate = FindCoordinate(k1,2,10,4);
+
+Console.WriteLine($"Intersection coordinates of two line: x = {Coordinate.Item1}, y = {Coordinate.Item2}");
