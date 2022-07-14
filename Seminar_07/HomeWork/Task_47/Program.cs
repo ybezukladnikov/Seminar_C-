7949d12eed75c39d5cdce065369e8ce58184d7b6
Yaroslav <yaros6666@yandex.ru>
2022-07-14 12:56:13 +0300

    add seminar 7

---
+++ Seminar_07/HomeWork/Task_47/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] CreateArray(int numOfLine, int numOfColumn)
+{
+    double [,] array = new double[numOfLine,numOfColumn];
+    Random rand = new Random();
+    
+    for (int i = 0; i<numOfLine; i++)
+    {
+        for(int j = 0; j<numOfColumn; j++)
+        {
+            array[i,j] = Math.Round(rand.NextDouble()*100,2);
+        }
+    }
+
+    return array;
+
+}
+
+void ShowArray(double [,] array)
+{
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        for(int j = 0; j<array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+"  ");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+
+Console.Write("Input number of line ");
+int numOfLine = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number of column ");
+int numOfColumn = Convert.ToInt32(Console.ReadLine());
+
+
+double [,] myArray = CreateArray(numOfLine,numOfColumn);
+
+ShowArray(myArray);
+
---
+++ Seminar_07/HomeWork/Task_50/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+
+
+                          
+void FindNum(int [,] array, int positionLine, int positionCol)
+{
+  
+    
+    if (positionLine>array.GetLength(0) || positionCol>array.GetLength(1)) 
+    {
+        Console.WriteLine("The numbers at such positions are not in the array");
+    }
+        
+    else
+    {
+       Console.WriteLine($"The numbers at such positions is: {array[positionLine,positionCol]}");
+    }    
+     
+}
+
+
+int [,] array = new int [,] {{1,2,3},{3,4,4},{67,34,1}};
+
+FindNum(array,4,0);
