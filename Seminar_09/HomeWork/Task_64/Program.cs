2a91b17eb84d11e01beb3fc820081555c89442d5
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-07-20 12:05:22 +1000

    add

--- Seminar_09/HomeWork/Task_64/Program.cs
+++ Seminar_09/HomeWork/Task_64/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Задача 64: Задайте значение N. 
 // Напишите программу, которая найдет факториал числа N рекурсивным методом.
 // N = 4 -> 24
+
+int Factorial(int n)
+{
+    if (n==1 || n==0) 
+        return 1;
+    else 
+        return n*Factorial(n-1);
+}
+
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int factorialOfNum = Factorial(n);
+
+Console.Write($"Factorial of number is {factorialOfNum}");
--- Seminar_09/HomeWork/Task_66/Program.cs
+++ Seminar_09/HomeWork/Task_66/Program.cs
@@ -2,3 +2,21 @@
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+int SumBetweenNum(int n, int m)
+{
+    if(n==m) 
+        return n;
+    else 
+        return n+SumBetweenNum(n+1,m);
+}
+
+Console.Write("Input first number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+int result = SumBetweenNum(n,m);
+Console.WriteLine($"Sum between numbers is {result}");
